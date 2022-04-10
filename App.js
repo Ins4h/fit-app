2dd9b47d368fb632f1c182168c449ea232384820
marcin-fijalkowski <mfijalkowski44@gmail.com>
2022-04-10 14:38:40 +0200

    Add firebase configuation

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import app from './firebase-config';
 
 export default function App() {
   return (
