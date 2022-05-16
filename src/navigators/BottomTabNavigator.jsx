fa01e89204670a0e35767688569c5b5da2210efd
Tomasz Pietkiewicz <insayy@gmail.com>
2022-05-16 11:38:04 +0200

    Add icons to bottom tab navigator

--- src/navigators/BottomTabNavigator.jsx
+++ src/navigators/BottomTabNavigator.jsx
@@ -1,6 +1,8 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import DashboardStack from "../views/DashboardView/DashboardStack";
 import WorkoutPlanStack from "../views/WorkoutPlanView/WorkoutPlanStack";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -11,9 +13,27 @@ const BottomTabNavigator = () => {
       screenOptions={{
         headerShown: false,
       }}
+      barStyle={{ backgroundColor: "#454545" }}
+      activeColor="#4CAF50"
     >
-      <Tab.Screen name="Dashboard" component={DashboardStack} />
-      <Tab.Screen name="Workout" component={WorkoutPlanStack} />
+      <Tab.Screen
+        name="Dashboard"
+        component={DashboardStack}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Workout"
+        component={WorkoutPlanStack}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="dumbbell" size={24} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
